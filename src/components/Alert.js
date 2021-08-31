b2d8a5a9d5d10e2f0c6fc356ba6ad99cd5288e5f
Irfan Ali Ansari <irfanaliansari35@gmail.com>
2021-08-31 19:48:33 +0530

    3rd commit
    Fixed some bugs in text preview area

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -2,13 +2,17 @@ import React from 'react'
 import _ from 'lodash'
 export default function Alert(props) {
 
-    const capitalize = (word)=>{
+    const capitalize = (word) => {
         return _.startCase(word);
     }
     return (
-        props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
-            <strong>{capitalize(props.alert.type)}</strong> {props.alert.msg}
+        <div style={{height : '50px'}}>
+            {
+                props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+                    <strong>{capitalize(props.alert.type)}</strong> {props.alert.msg}
 
+                </div>
+            }
         </div>
     );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -49,7 +49,7 @@ export default function TextForm(props) {
             <div className="container" style={{color : props.mode==='dark'?'white' : 'black'}}>
                 <h2>{props.heading}</h2>
                 <div className="mb-3">
-                    <textarea className="form-control" value={text} onChange={handleChange} style={{backgroundColor : props.mode==='dark'?'grey' : 'white',color : props.mode==='dark'?'white' : 'black'}} id="myBox" rows="7"></textarea>
+                    <textarea className="form-control" value={text} onChange={handleChange} style={{backgroundColor : props.mode==='dark'?'#021524' : 'white',color : props.mode==='dark'?'white' : 'black'}} id="myBox" rows="7"></textarea>
                     <button className="btn btn-primary my-3 mx-2" onClick={handleUpClick}> Convert to Uppercase</button>
                     <button className="btn btn-primary my-3 mx-2" onClick={handleLoClick}> Convert to Lowercase</button>
                     <button className="btn btn-primary my-3 mx-2" onClick={handleScClick}> Capitalize</button>
@@ -61,8 +61,8 @@ export default function TextForm(props) {
             </div>
             <div className="container" style={{color : props.mode==='dark'?'white' : 'black'}}>
                 <h2>Your Text Summary</h2>
-                <p>{text.split(" ").length} words and {text.length} characters</p>
-                <p>{0.008 * text.split(" ").length} Minutes to read </p>
+                <p>{text.split(" ").filter((element)=> {return element!==""}).length} words and {text.length} characters</p>
+                <p>{0.008 * text.split(" ").filter((element)=> {return element!==""}).length} Minutes to read </p>
                 <h3>Preview</h3>
                 <p>{text.length>0 ? text : "Enter something in the textbox above to preview here"}</p>
             </div>
